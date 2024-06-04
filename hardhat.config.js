e8db35d757111b444ac95b2e29a9dcfd78565f51
RjtSinghal <singhal.rajat.1995@gmail.com>
2024-06-04 19:24:01 +0530

    hardhat config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_KEY = "FmGDI4UcMBANwWCqwhSao4bikTeERzO1";
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 
-const SEPOLIA_PRIVATE_KEY = "6da53b01716bd6b34e6f4c11500c898760a4f2528604e0419c45f8e884652296";
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
-const ETHERSCAN_API_KEY = "17MN6P8B8ASINI8E33EG18AC2FEAZ46KMJ";
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   networks: {
     sepolia: {
+      // url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      // accounts: [SEPOLIA_PRIVATE_KEY]
+
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
   },
   etherscan: {
+    // apiKey: ETHERSCAN_API_KEY,
     apiKey: ETHERSCAN_API_KEY,
   },
   solidity: "0.8.20",
